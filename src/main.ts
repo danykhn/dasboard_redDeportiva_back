8387599e4d6d874f8fcd166af5adf1a9859f1181
danykhn <danjel2014@gmail.com>
2025-11-12 21:47:28 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -42,11 +42,15 @@ async function bootstrap() {
   // CORS habilitado para el frontend
   app.enableCors({
     origin: [
+      'https://dasboard-red-deportiva-front.vercel.app',
       'https://dasboard-red-deportiva-front-danykhns-projects.vercel.app',
+      'https://dasboard-red-deportiva-front.vercel.app/',
       'http://localhost:3000',
       'http://localhost:5173',
     ],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   });
 
   await app.listen(process.env.PORT ?? 3009);
